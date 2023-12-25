f484fab08f8420c1e7043f766c953c86ce16737f
Hyerin <125418818+ooherin@users.noreply.github.com>
2023-12-26 03:08:36 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -4,8 +4,8 @@ function solution(n) {
     dp[1]=1;
     dp[2]=2;
     for(var i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2] %1234567;
+        dp[i]=dp[i-1]+dp[i-2] % 1234567;
     }
     answer=dp[n];
-    return answer%1234567;
+    return answer  % 1234567;
 }
