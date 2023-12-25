e8585ac5493e2a1dcd73fbe7d9e3d146047fb90b
Hyerin <125418818+ooherin@users.noreply.github.com>
2023-12-26 02:40:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,28 +1,11 @@
 function solution(n) {
-    const MOD = 1234567;
-    let answer = BigInt(1);
-
-    // 가능한 2의 개수 구하기
-    const twoCount = Math.floor(n / 2);
-    let i = BigInt(1);
-
-    while (i <= twoCount) {
-        // 1의 개수
-        const oneCount = BigInt(n) - BigInt(2) * i;
-        answer += total(BigInt(n) - i) / (total(oneCount) * total(i));
-        // 2의 개수
-        i++;
+    var answer = 0;
+    var dp=[];
+    dp[1]=1;
+    dp[2]=2;
+    for(var i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2] %1234567;
     }
-
-    return Number(answer % BigInt(MOD));
-}
-
-function total(n) {
-    let i = BigInt(1);
-    let answer = BigInt(1);
-    while (i <= n) {
-        answer *= i;
-        i++;
-    }
-    return answer;
+    answer=dp[n];
+    return answer%1234567;
 }
