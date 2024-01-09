314eb5e89101cd3a6628e28c8532faa49dd1b353
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-01-09 16:54:52 +0900

    [level 2] Title: 튜플, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let sliced = '},' + s.slice(1, s.length -1) + ',{'
+    sliced = sliced.split('},{').filter(e => e.length > 0).map((str) => {
+        let splited = str.split(',');
+        return splited
+    })
+    sliced = sliced.map((arr) => arr.map((e) => Number(e)));
+    const sorted = sliced.sort((a,b) => a.length - b.length);
+    let set = new Set();
+    sorted.forEach((arr,index) => {
+        arr.forEach((e) => {
+             set.add(e)
+        })
+    })
+    return Array.from(set);
+}
