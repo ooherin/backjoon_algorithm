dcff1012d2b8accf96008d991b6191ea360879f0
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-01-15 11:47:02 +0900

    Update 옹알이 （1）.js

--- 프로그래머스/Lv.0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/Lv.0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -9,3 +9,21 @@ function solution(babbling) {
 
   return answer;
 }
+
+//원래 풀이
+//단어가 반복되서 등장하는 경우는 없기에 굳이 같은 단어가 있는지를 확인하지 않아도 된다. 
+function solution(babbling) {
+    var answer = 0;
+    const words = ["aya","ye","woo","ma"];
+    const changed = babbling.map((babble) => {
+        words.forEach((word,index) => {
+            babble = babble.replace(word,index)
+        })
+        const regex = /^[0-9]+$/
+        if(regex.test(babble)){
+            answer++
+        }
+    })
+    
+    return answer;
+}
