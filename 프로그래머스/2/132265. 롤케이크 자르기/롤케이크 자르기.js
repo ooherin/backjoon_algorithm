3c3763cfcade26ecd0923262a88b16cd95a3d8ec
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-02-09 23:36:16 +0900

    Update 롤케이크 자르기.js

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -32,3 +32,13 @@ function solution(topping) {
     })
     return answer;
 }
+
+//처음 알고리즘 : topping1Count, topping2Count를 먼저 선언하는 게 아니라, 매번 반복할 때마다 변수를 만들어 length를 세주는 것
+//시간 초과 
+//불필요한 객체 생성 및 삭제: 매 반복마다 topping1과 topping2의 크기를 계산하고, 
+//객체의 추가 및 삭제가 반복됩니다. 이는 메모리 및 연산 비용을 증가시킵니다.
+//객체의 키의 개수가 많다면 충분히 메모리 증가..
+    const topping1Count = Object.keys(topping1).length
+    const topping2Count = Object.keys(topping2).length
+    if(topping1Count === topping2Count){
+        answer++;}
