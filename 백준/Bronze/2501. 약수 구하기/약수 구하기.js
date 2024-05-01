cebed4155c048025c89faf26d7dbcdfb7b305be4
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-05-01 23:23:27 +0900

    [Bronze III] Title: 약수 구하기, Time: 140 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.js
@@ -0,0 +1,18 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [target, sequence] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .split(" ")
+  .map(Number);
+
+const sqrt = Math.sqrt(target);
+
+const set = new Set();
+for (let i = 1; i <= sqrt; i++) {
+  if (target % i === 0) {
+    set.add(target / i);
+    set.add(i);
+  }
+}
+const arr = [...set].sort((a, b) => a - b);
+console.log(arr[sequence - 1] ?? 0);
