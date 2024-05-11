cbbc2cf5163f4dc9c74d2c4e63882627678a033d
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-05-12 02:30:44 +0900

    [level 2] Title: 귤 고르기, Time: 61.14 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#qna) 
 
 ### 성능 요약
 
-메모리: 50.6 MB, 시간: 72.75 ms
+메모리: 50.7 MB, 시간: 61.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 23:32:8
+2024년 05월 12일 02:30:41
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,14 +1,16 @@
 function solution(k, tangerine) {
-    var answer = 0;
+    let answer = 0;
+    let total = 0;
     let obj = tangerine.reduce((acc,cur)=>{
         acc[cur]  =  (acc[cur] || 0 ) + 1;
         return acc;
     },{})
-    let arr = Object.values(obj).sort((a,b) => b-a);
-    let total = 0;
-    while(total < k){
-        total += arr[answer];
-        answer++
-    }
-    return answer;
+   const sortedObj = Object.values(obj).sort((a,b) =>  b- a);
+ 
+   for(let i = 0; i < sortedObj.length; i++){
+       if(total >= k) break;
+       answer++;
+       total += sortedObj[i];
+   }
+    return answer; 
 }
