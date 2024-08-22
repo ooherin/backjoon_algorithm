25f6559bd24acbdd8c785428ad0f4c0f14c15b41
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-08-23 00:38:32 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.js
@@ -0,0 +1,14 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const schools = {
+  NLCS: "North London Collegiate School",
+  BHA: "Branksome Hall Asia",
+  KIS: "Korea International School",
+  SJA: "St. Johnsbury Academy",
+};
+
+console.log(schools[input]);
