59511a2ea60e3a6ccd97903ca9a5acb61bb09dd8
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-08-27 21:42:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 26.65 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,47 +1,4 @@
-const directions = [
-  [0, -1],
-  [0, 1],
-  [1, 0],
-  [-1, 0],
-];
-
 function solution(maps) {
-  //x축 길이
-  const N = maps[0].length;
-  //y축 길이
-  const M = maps.length;
-
-  let answer = -1;
-  const newMaps = [new Array(N).fill(0), ...maps].map((line) => [0, ...line]);
-  const visited = Array.from({ length: M + 1 }, () => Array(N + 1).fill(false));
-
-  const queue = [[1, 1, 1]];
-  while (queue.length) {
-    const [cy, cx, count] = queue.shift();
-
-    for (const direction of directions) {
-      const [dx, dy] = direction;
-      const nx = dx + cx;
-      const ny = dy + cy;
-
-      if (ny === M && nx === N) {
-        return count + 1;
-      }
-
-      if (
-        ny >= 1 &&
-        ny <= M &&
-        nx >= 1 &&
-        nx <= N &&
-        newMaps[ny][nx] === 1 &&
-        !visited[ny][nx]
-      ) {
-        queue.push([ny, nx, count + 1]);
-        visited[ny][nx] = true;
-      }
-    }
-  }
-
-  return answer;
+    var answer = 0;
+    return answer;
 }
-
