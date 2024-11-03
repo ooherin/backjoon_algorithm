cd411367239333c1ee43df6102253bd72d6a1299
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-11-03 09:12:32 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,16 +1,39 @@
-function solution(n, computers) {
-    //하나씩 순회해서 서로 연결되어 있지 않고
-    //연결된 다른 숫자가 그 컴퓨터에 연결되어 있지 않은 경우 
-    // + 1
-    var answer = 0;
-    let networks = Array.from({length : computers.length}).map((e) => [])
-   
-    for(let i =0; i < computers.length; i++){
-        for(let j = 0 ; j < computers.length; j++){
-            if(computers[i][j]){
-                networks[i].push(j)
-            }
-        }
-    }
-    return networks;
-}
+const solution = (n, computers) => {
+  const networks = computers.map((e, index) => {
+    const linked = [];
+    e.forEach((other, otherIndex) => {
+      if (other === 1) {
+        linked.push(otherIndex);
+      }
+    });
+    return linked;
+  });
+    
+  const visited = new Array(n).fill(false);
+
+  //트리를 순회하면서 해당 노드를 계속 찍기
+  //만약 더이상 연결이 없으면 + 1
+  let answer = 0;
+    
+  const dfs = (node) => {
+    //현재 노드에서 연결된 노드 하나씩 다 탐색
+      const currentNodeNetwork = networks[node];
+      
+          currentNodeNetwork.forEach((linkedNode) => {
+          if(!visited[linkedNode]) {
+              visited[linkedNode] = true
+              dfs(linkedNode);   
+          }
+      })  
+  }
+  
+  
+  for(let i = 0 ; i < networks.length ;i++){
+      if(visited[i]) continue;
+      dfs(i);
+      answer++;
+  }
+    
+return answer
+};
+
